2b384aa0a4253a3a06c628cf37f247d6644d1389
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-10-17 14:31:34 +0300

    feat: :zap: update our app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import "./style.css";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import BrowseRange from "./components/BrowseRange";
-import Products from "./components/Products";
-import Footer from "./components/Footer";
+import "./media.css";
+import HomePage from "./Pages/HomePage";
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <Hero />
-      <BrowseRange />
-      <Products />
-      <Footer />
+      <HomePage />
     </div>
   );
 };
