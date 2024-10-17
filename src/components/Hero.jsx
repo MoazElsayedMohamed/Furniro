107ad23777e69183170e674221656eda21267305
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-10-17 14:26:41 +0300

    feat: :memo: build the hero component of the page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-container">
+        <h3 className="hero-new">new arrival</h3>
+        <h2 className="hero-header">Discover Our New Collection</h2>
+        <p className="hero-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, dolore
+          aspernatur aut voluptates veritatis hic maiores nihil qui
+          reprehenderit.
+        </p>
+        <button className="hero-button">buy now</button>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
