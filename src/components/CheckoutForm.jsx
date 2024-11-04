3ee1a80230895798f2286fbcf4a9402ed8b63252
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-11-04 13:13:40 +0200

    feat: :zap: update checkout handling click for sumbitting with clicking or submitting

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,31 +1,144 @@
-import React from "react";
-import { Form } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import FormRow from "./FormRow";
-import FormRowSelector from "./FormRowSelector";
+import { useCart } from "../context/CartContext";
+import { toast } from "sonner";
+import { useAuth } from "../context/AuthContext";
+import { getCode } from "country-list";
+import CheckoutPrice from "./CheckoutPrice";
 
 const CheckoutForm = () => {
+  const [checkoutInput, setCheckoutInput] = useState({
+    first_name: "",
+    last_name: "",
+    country_code: "",
+    company: "",
+    address: "",
+    city: "",
+    province: "",
+    zip_code: "",
+  });
+  const [countryName, setCountryName] = useState("");
+  let { item } = useCart();
+  if (item.length === 1) {
+    item = item[0];
+  }
+  if (item.length > 1) {
+    item.map((single) => {
+      item.id = single.id;
+      item.count = single.count;
+    });
+  }
+
+  const { token } = useAuth();
+  const navigate = useNavigate();
+
+  function handleChange(e) {
+    setCheckoutInput((prevInput) => ({
+      ...prevInput,
+      [e.target.id]: e.target.value,
+      country_code: getCode(countryName),
+    }));
+  }
+
+  const requestData = {
+    data: {
+      ...checkoutInput,
+      order_items: [
+        {
+          product: item.id,
+          count: item.count,
+        },
+      ],
+    },
+  };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:1337/api/checkout`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestData),
+      });
+
+      if (!response.ok) {
+        throw new Error("error");
+      }
+      const data = await response.json();
+      console.log(data);
+
+      toast.success("checkout successfully");
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <Form className="flex flex-col gap-8">
-      <h2 className="text-5xl font-bold mb-8">Billing details</h2>
-      <div className="flex gap-16">
-        <FormRow id="firstname" type="text" label="First Name" />
-        <FormRow id="lastname" type="text" label="Last Name" />
-      </div>
-      <FormRow id="countryname" type="text" label="Country Name (Optional)" />
-      <FormRowSelector
-        id="country"
-        label="country / region"
-        option="sir lanka"
-      />
-      <FormRow id="streetaddress" type="text" label="Street Address" />
-      <FormRow id="town" type="text" label="Town / City" />
-      <FormRowSelector
-        id="province"
-        label="Province"
-        option="western province"
-      />
-      <FormRow id="zipcode" type="text" label="ZIP Code" />
-    </Form>
+    <>
+      <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
+        <h2 className="text-5xl font-bold mb-8">Billing details</h2>
+        <div className="flex gap-16">
+          <FormRow
+            id="first_name"
+            type="text"
+            label="First Name"
+            onChange={handleChange}
+          />
+          <FormRow
+            id="last_name"
+            type="text"
+            label="Last Name"
+            onChange={handleChange}
+          />
+        </div>
+        <FormRow
+          id="company"
+          type="text"
+          label="Company Name (Optional)"
+          onChange={handleChange}
+        />
+        <FormRow
+          id="country_code"
+          label="country / region"
+          option="sir lanka"
+          onChange={(e) => setCountryName(e.target.value)}
+        />
+        <FormRow
+          id="address"
+          type="text"
+          label="Street Address"
+          onChange={handleChange}
+        />
+        <FormRow
+          id="city"
+          type="text"
+          label="Town / City"
+          onChange={handleChange}
+        />
+        <FormRow
+          id="province"
+          label="Province"
+          option="western province"
+          onChange={handleChange}
+        />
+        <FormRow
+          id="zip_code"
+          type="text"
+          label="ZIP Code"
+          onChange={handleChange}
+        />
+        <button type="submit" className="hidden">
+          submit
+        </button>
+      </form>
+      <CheckoutPrice handleSubmit={handleSubmit} />
+    </>
   );
 };
 
--- src/components/CheckoutPrice.jsx
+++ src/components/CheckoutPrice.jsx
@@ -1,26 +1,68 @@
 import React from "react";
 import { useCart } from "../context/CartContext";
 import { Button } from "./ui/button";
+import { useProduct } from "../context/ProductContext";
 
-const CheckoutPrice = () => {
-  const { items, totalPrice } = useCart();
-  const { name, price, count } = items();
+const CheckoutPrice = ({ handleSubmit }) => {
+  const { productCount } = useProduct();
+  let { item, totalPrice } = useCart();
+  if (item.length === 1) {
+    item = item[0];
+  }
+  const { name, price, count } = item;
 
   return (
     <div className="w-5/12">
       <div className="flex gap-80 border-b border-gray-300 pb-4">
         <div className="flex flex-col gap-6">
           <h1 className="text-4xl font-semibold capitalize">product</h1>
-          <h2 className="text-2xl">
-            <span className="text-gray-400 mr-4">{name}</span> x {count}
-          </h2>
+          {item.length > 1 ? (
+            item.map((item) => {
+              const name = item.name;
+              return (
+                <h2 className="text-2xl">
+                  <span className="text-gray-400 mr-4">{name}</span> x{" "}
+                  {productCount}
+                </h2>
+              );
+            })
+          ) : (
+            <h2 className="text-2xl">
+              {name || count ? (
+                <>
+                  <span className="text-gray-400 mr-4">{name}</span> x{" "}
+                  {productCount}
+                </>
+              ) : (
+                ""
+              )}
+            </h2>
+          )}
           <h3 className="text-2xl capitalize">subtotal</h3>
           <h4 className="text-2xl capitalize">total</h4>
         </div>
         <div className="flex flex-col gap-6">
           <h1 className="text-4xl capitalize font-semibold">subtotal</h1>
-          <h1 className="text-2xl">Rs.{price ? price : 0}.000.00</h1>
-          <h1 className="text-2xl">Rs.{price ? price : 0}.000.00</h1>
+
+          {item.length > 1 ? (
+            item.map((item) => {
+              const price = item.price;
+              return (
+                <h1 className="text-2xl">
+                  Rs.{price ? Math.floor(price * productCount) : 0}.000.00
+                </h1>
+              );
+            })
+          ) : (
+            <h1 className="text-2xl">
+              {name || count
+                ? `Rs.${price ? Math.floor(price * productCount) : 0}.000.00`
+                : ""}
+            </h1>
+          )}
+          <h1 className="text-2xl">
+            Rs.{totalPrice ? totalPrice() : 0}.000.00
+          </h1>
           <h1 className="text-3xl text-primary font-bold">
             Rs.{totalPrice()}.000.00
           </h1>
@@ -50,7 +92,7 @@ const CheckoutPrice = () => {
           described in our privacy policy.
         </p>
         <div className="flex justify-center mt-12">
-          <Button variant="outline2" size="lg">
+          <Button variant="outline2" size="lg" onClick={handleSubmit}>
             Place order
           </Button>
         </div>
